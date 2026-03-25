5864739cb5ee38308dfb2dad63c697d4ba12e7f9
Delbighily <d.elbighily@gmail.com>
2026-03-25 12:52:24 +0200

    Initial commit

---
+++ src/test/java/com/planner/TestCase.java
@@ -0,0 +1,14 @@
+package com.planner;
+
+import org.testng.annotations.*;
+import static io.restassured.RestAssured.*;
+public class TestCase {
+
+
+    @Test
+    public void test(){
+        given()
+                .when()
+                .then();
+    }
+}
